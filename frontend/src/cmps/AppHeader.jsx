597ec675f2df9819fc9542a65b72eda024d83d89
Tomer Neeman <tomer@alink.co.il>
2024-11-08 15:25:07 +0200

    Header top section without functionality

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -1,11 +1,24 @@
 import { Logo } from './Logo.jsx'
 
 export function AppHeader() {
-	
+
 
 	return (
 		<header className="app-header full">
-		<Logo/>
+			<section className="top-header">
+				<Logo />
+				<section className='stay-experience-btns'>
+					<button className="btn-active">Stays</button>
+					<button className="btn">Experiences</button>
+				</section>
+				<div>
+				<button className="btn btn-active airbnb-your-home">Airbnb your home</button>
+				<button className="btn btn-active"><img className='i18' src="/src/assets/img/i18.svg" alt="" /></button>
+				<button className="btn btn-shadow"><img className='burger' src="/src/assets/img/burger.svg" alt="" /><img className='avatar' src="/src/assets/img/avatar.svg" alt="" /></button>
+					
+				</div>
+			</section>
+
 		</header>
 	)
 }
