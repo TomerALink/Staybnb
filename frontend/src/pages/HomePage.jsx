be7632a91765366d7aa14e5589d01e006fb9ffe5
Tomer Neeman <tomer@alink.co.il>
2024-11-14 12:55:38 +0200

    Stay filter advanced (not done yet)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,11 +3,7 @@ import { StayList } from "../cmps/StayList";
 export function HomePage() {
   return (
     <section className="home-page">
-
-{/* //      <h1>Staybnb</h1>
-//      <StayPreview/> */}
-
-<StayList/>
+      <StayList />
     </section>
   )
 }
