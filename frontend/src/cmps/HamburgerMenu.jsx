88124dac7707452d3a2862fabfe814eb58ee1613
Tomer Neeman <tomer@alink.co.il>
2024-11-16 11:21:36 +0200

    Add Hamburger Menu

---
+++ frontend/src/cmps/HamburgerMenu.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+export function HamburgerMenu({setShowMenu, menuBtn}) {
+    
+
+    const menuRef =useRef(null)
+
+	useEffect(() => {
+		// Handler to call when clicking outside
+		function handleClickOutside(event) {
+            console.log(event.target)
+		  if (menuRef.current && !menuBtn.current.contains(event.target) && !menuRef.current.contains(event.target)) {
+			setShowMenu(false)
+		  }
+		}
+	
+		// Bind the event listener
+		window.addEventListener("mousedown", handleClickOutside)
+		return () => {
+		  // Unbind the event listener on cleanup
+		  window.removeEventListener("mousedown", handleClickOutside)
+		}
+	  }, [])
+
+    return (
+        (
+            <div ref={menuRef} className="hamburger-menu" onClick={(ev) => ev.stopPropagation()}>
+                <div>Log in</div>
+                <div>Sign up</div>
+                <br />
+                <hr/>
+                <div>Gift cards</div>
+                <Link to="/stay/add"><div>Airbnb your home</div></Link>
+                <div>Host an experience</div>
+                <div>Help Center</div>
+            </div>
+        )
+
+    )
+}
